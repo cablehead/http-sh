33d958164419d6a6095dfef279cbcde5227ec2a5
Andy Gayton <andy@thecablelounge.com>
2022-08-28 14:42:23 -0400

    streaming response

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,20 @@
-use futures::TryStreamExt;
-use hyper::service::{make_service_fn, service_fn};
 use std::convert::Infallible;
 use std::net::SocketAddr;
+
+use futures::TryStreamExt;
+use hyper::service::{make_service_fn, service_fn};
 use tokio::signal::unix::{signal, SignalKind};
 
 async fn hello_world(
     req: hyper::Request<hyper::Body>,
 ) -> Result<hyper::Response<hyper::Body>, Infallible> {
-    let mut response = hyper::Response::new(hyper::Body::empty());
+    let mut p = tokio::process::Command::new("./run.sh")
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .expect("failed to spawn");
 
-    match (req.method(), req.uri().path()) {
-        (&hyper::Method::GET, "/") => {
-            *response.body_mut() = hyper::Body::from("Try POSTing data to /echo");
-        }
-
-        (&hyper::Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        }
-
-        // Yet another route inside our match block...
-        (&hyper::Method::POST, "/echo/uppercase") => {
-            // This is actually a new `futures::Stream`...
-            let mapping = req.into_body().map_ok(|chunk| {
-                chunk
-                    .iter()
-                    .map(|byte| byte.to_ascii_uppercase())
-                    .collect::<Vec<u8>>()
-            });
-
-            // Use `Body::wrap_stream` to convert it to a `Body`...
-            *response.body_mut() = hyper::Body::wrap_stream(mapping);
-        }
-
-        _ => {
-            *response.status_mut() = hyper::StatusCode::NOT_FOUND;
-        }
-    };
-
+    let body = hyper::Body::wrap_stream(tokio_util::io::ReaderStream::new(p.stdout.unwrap()));
+    let response = hyper::Response::new(body);
     Ok(response)
 }
 
@@ -52,16 +30,11 @@ async fn shutdown_signal() {
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
     let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
         Ok::<_, Infallible>(service_fn(hello_world))
     });
-
     let server = hyper::Server::bind(&addr).serve(make_svc);
-
     let graceful = server.with_graceful_shutdown(shutdown_signal());
-
     if let Err(e) = graceful.await {
         eprintln!("server error: {}", e);
     }
