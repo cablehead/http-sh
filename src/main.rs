28400f4ad7431ae61ce03110f0cf5866138791cc
Andy Gayton <andy@thecablelounge.com>
2022-08-31 13:58:30 -0400

    add not found handler, begin to add dedicated POST handler

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,28 @@ use std::net::SocketAddr;
 
 use hyper::service::{make_service_fn, service_fn};
 use tokio::signal::unix::{signal, SignalKind};
+use hyper::{Request, Response, Body, StatusCode};
 
 async fn handler(
-    req: hyper::Request<hyper::Body>,
-) -> Result<hyper::Response<hyper::Body>, hyper::http::Error> {
+    req: Request<Body>,
+) -> Result<Response<Body>, hyper::http::Error> {
+    match req.method() {
+        &hyper::Method::GET => println!("hi: GET"),
+        &hyper::Method::POST => println!("hi: POST"),
+        _ => {
+            return Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body("not found".into());
+        }
+    }
+
     let p = tokio::process::Command::new("./run.sh")
         .stdout(std::process::Stdio::piped())
         .spawn()
         .expect("failed to spawn");
 
-    let body = hyper::Body::wrap_stream(tokio_util::io::ReaderStream::new(p.stdout.unwrap()));
-    let response = hyper::Response::builder()
+    let body = Body::wrap_stream(tokio_util::io::ReaderStream::new(p.stdout.unwrap()));
+    let response = Response::builder()
         .header("Content-Type", "text/event-stream")
         .header("Access-Control-Allow-Origin", "http://localhost:8000")
         .body(body);
@@ -41,13 +52,35 @@ async fn main() {
 }
 
 #[tokio::test]
-async fn test_handler() {
-    let req = hyper::Request::get("https://www.rust-lang.org/")
-        .body(hyper::Body::empty())
+async fn test_handler_get() {
+    let req = Request::get("https://www.rust-lang.org/")
+        .body(Body::empty())
         .unwrap();
     let resp = handler(req).await.unwrap();
     assert_eq!(
         resp.headers().get("content-type").unwrap(),
         "text/event-stream",
     );
+}
+
+#[tokio::test]
+async fn test_handler_post() {
+    let req = Request::get("https://www.rust-lang.org/")
+        .body(Body::empty())
+        .unwrap();
+    let resp = handler(req).await.unwrap();
+    assert_eq!(
+        resp.headers().get("content-type").unwrap(),
+        "text/event-stream",
+    );
+}
+
+#[tokio::test]
+async fn test_handler_404() {
+    let req = Request::delete("https://www.rust-lang.org/")
+    .body(Body::empty())
+    .unwrap();
+    let resp = handler(req).await.unwrap();
+    println!("RESP: {:?}", resp);
+    assert_eq!(resp.status(), StatusCode::NOT_FOUND);
 }
