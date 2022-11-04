9ce43ba5d012990e39f30fc030eb948c3c46d0ad
Andy Gayton <andy@thecablelounge.com>
2022-11-04 15:19:23 -0400

    strip back to the basics

--- src/main.rs
+++ src/main.rs
@@ -51,114 +51,57 @@ async fn handler(
     command: &String,
     args: &Vec<String>,
 ) -> Result<Response<Body>, hyper::http::Error> {
-    println!("{:?} {} {:?}", req, command, args);
-    match req.method() {
-        &hyper::Method::GET => {
-            let mut args = vec!["./stream", "cat", "--follow", "--sse"];
-            if let Some(last_id) = req.headers().get("Last-Event-ID") {
-                args.push("--last-id");
-                args.push(last_id.to_str().unwrap());
-            }
+    /*
+    let params: HashMap<String, String> = req
+        .uri()
+        .query()
+        .map(|v| {
+            url::form_urlencoded::parse(v.as_bytes())
+                .into_owned()
+                .collect()
+        })
+        .unwrap_or_else(HashMap::new);
+    */
 
-            println!("{:?}", args);
-
-            let p = tokio::process::Command::new("xs-2")
-                .args(args)
-                .stdin(std::process::Stdio::null())
-                .stdout(std::process::Stdio::piped())
-                .spawn()
-                .expect("failed to spawn");
-
-            let body = Body::wrap_stream(tokio_util::io::ReaderStream::new(p.stdout.unwrap()));
-            let resp = Response::builder()
-                .header("Content-Type", "text/event-stream")
-                .header("Access-Control-Allow-Origin", "http://localhost:8000")
-                .body(body);
-            return resp;
-        }
-
-        &hyper::Method::POST => {
-            let (parts, body) = req.into_parts();
-
-            let re = regex::Regex::new(r"^/pipe/(?P<id>\d+)$").unwrap();
-            let id = re
-                .captures(parts.uri.path())
-                .unwrap()
-                .name("id")
-                .unwrap()
-                .as_str();
-
-            let body = hyper::body::to_bytes(body).await.unwrap().to_vec();
-            let body = std::str::from_utf8(&body).unwrap();
-
-            let p = tokio::process::Command::new("xs-2")
-                .args(vec!["./stream", "pipe", id, "--", "bash", "-c", body])
-                .stdin(std::process::Stdio::null())
-                .stdout(std::process::Stdio::piped())
-                .spawn()
-                .expect("failed to spawn");
-
-            let body = Body::wrap_stream(tokio_util::io::ReaderStream::new(p.stdout.unwrap()));
-            let resp = Response::builder()
-                .header("Content-Type", "text/plain")
-                .header("Access-Control-Allow-Origin", "http://localhost:8000")
-                .body(body);
-            return resp;
-        }
-
-        &hyper::Method::OPTIONS => {
-            return Response::builder()
-                .header("Access-Control-Allow-Origin", "http://localhost:8000")
-                .header("Access-Control-Allow-Methods", "*")
-                .status(StatusCode::NO_CONTENT)
-                .body(Body::empty());
-        }
-
-        &hyper::Method::PUT => {
-            let params: HashMap<String, String> = req
-                .uri()
-                .query()
-                .map(|v| {
-                    url::form_urlencoded::parse(v.as_bytes())
-                        .into_owned()
-                        .collect()
-                })
-                .unwrap_or_else(HashMap::new);
-
-            let mut cmd = tokio::process::Command::new("xs-2");
-            cmd.args(vec!["./stream", "put"]);
-            params
-                .get("source_id")
-                .map(|x| cmd.args(vec!["--source-id", x]));
-            params.get("topic").map(|x| cmd.args(vec!["--topic", x]));
-            params
-                .get("attribute")
-                .map(|x| cmd.args(vec!["--attribute", x]));
-            let mut p = cmd
-                .stdin(std::process::Stdio::piped())
-                .spawn()
-                .expect("failed to spawn");
-
-            let body = req
-                .into_body()
-                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e));
-            let mut body = tokio_util::io::StreamReader::new(body);
-            let mut stdin = p.stdin.take().expect("failed to open stdin");
-            tokio::io::copy(&mut body, &mut stdin)
-                .await
-                .expect("pipe failed");
-            return Response::builder()
-                .header("Content-Type", "text/plain")
-                .header("Access-Control-Allow-Origin", "http://localhost:8000")
-                .body("ok".into());
-        }
-
-        _ => {
-            return Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body("not found".into());
-        }
+    /*
+    if let Some(last_id) = req.headers().get("Last-Event-ID") {
+        args.push("--last-id");
+        args.push(last_id.to_str().unwrap());
     }
+    */
+
+    /*
+    let (parts, body) = req.into_parts();
+    let re = regex::Regex::new(r"^/pipe/(?P<id>\d+)$").unwrap();
+    let id = re
+        .captures(parts.uri.path())
+        .unwrap()
+        .name("id")
+        .unwrap()
+        .as_str();
+    */
+
+    let mut p = tokio::process::Command::new(command)
+        .args(args)
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .expect("failed to spawn");
+
+    let body = req
+        .into_body()
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e));
+    let mut body = tokio_util::io::StreamReader::new(body);
+    let mut stdin = p.stdin.take().expect("failed to open stdin");
+    tokio::io::copy(&mut body, &mut stdin)
+        .await
+        .expect("pipe failed");
+
+    let body = Body::wrap_stream(tokio_util::io::ReaderStream::new(p.stdout.unwrap()));
+    let resp = Response::builder()
+        .header("Content-Type", "text/plain")
+        .body(body);
+    return resp;
 }
 
 async fn shutdown_signal() {
@@ -172,15 +115,16 @@ async fn shutdown_signal() {
 
 #[tokio::test]
 async fn test_handler() {
-    /*
-    let req = Request::get("https://www.rust-lang.org/")
+    let req = Request::post("https://www.rust-lang.org/")
         .header("Last-Event-ID", 5)
-        .body(Body::empty())
+        .body("zebody".into())
         .unwrap();
-    let resp = handler(req).await.unwrap();
-    assert_eq!(
-        resp.headers().get("content-type").unwrap(),
-        "text/event-stream",
-    );
-    */
+    let resp = handler(req, &"bash".into(), &vec!["-c".into(), "cat".into()])
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::OK);
+    assert_eq!(resp.headers().get("content-type").unwrap(), "text/plain");
+    let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+    let body = std::str::from_utf8(&body).unwrap();
+    assert_eq!(body, "zebody");
 }
