bb1cb17808dd7c3a86fe1db4f5bccab54115f966
Andy Gayton <andy@thecablelounge.com>
2022-11-08 22:05:05 -0500

    wait for stdin before returning the http response

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,37 @@ use hyper::{Body, Request, Response, StatusCode};
 
 use clap::{AppSettings, Parser};
 
+/*
+ * todo:
+let params: HashMap<String, String> = req
+    .uri()
+    .query()
+    .map(|v| {
+        url::form_urlencoded::parse(v.as_bytes())
+            .into_owned()
+            .collect()
+    })
+    .unwrap_or_else(HashMap::new);
+*/
+
+/*
+if let Some(last_id) = req.headers().get("Last-Event-ID") {
+    args.push("--last-id");
+    args.push(last_id.to_str().unwrap());
+}
+*/
+
+/*
+let (parts, body) = req.into_parts();
+let re = regex::Regex::new(r"^/pipe/(?P<id>\d+)$").unwrap();
+let id = re
+    .captures(parts.uri.path())
+    .unwrap()
+    .name("id")
+    .unwrap()
+    .as_str();
+*/
+
 #[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
 #[clap(global_setting(AppSettings::DisableHelpSubcommand))]
@@ -33,7 +64,9 @@ async fn main() {
         let args = args.clone();
         let svc_fn = service_fn(move |req| {
             let args = args.clone();
-            async move { handler(req, &args.command, &args.args).await }
+            async move {
+                Ok::<Response<Body>, Infallible>(handler(req, &args.command, &args.args).await)
+            }
         });
         async move { Ok::<_, Infallible>(svc_fn) }
     });
@@ -46,43 +79,8 @@ async fn main() {
     }
 }
 
-async fn handler(
-    req: Request<Body>,
-    command: &String,
-    args: &Vec<String>,
-) -> Result<Response<Body>, hyper::http::Error> {
+async fn handler(req: Request<Body>, command: &String, args: &Vec<String>) -> Response<Body> {
     println!("{:?}", req);
-    /*
-    let params: HashMap<String, String> = req
-        .uri()
-        .query()
-        .map(|v| {
-            url::form_urlencoded::parse(v.as_bytes())
-                .into_owned()
-                .collect()
-        })
-        .unwrap_or_else(HashMap::new);
-    */
-
-    /*
-    if let Some(last_id) = req.headers().get("Last-Event-ID") {
-        args.push("--last-id");
-        args.push(last_id.to_str().unwrap());
-    }
-    */
-
-    /*
-    let (parts, body) = req.into_parts();
-    let re = regex::Regex::new(r"^/pipe/(?P<id>\d+)$").unwrap();
-    let id = re
-        .captures(parts.uri.path())
-        .unwrap()
-        .name("id")
-        .unwrap()
-        .as_str();
-    */
-
-    let (parts, body) = req.into_parts();
 
     fn headers_to_hashmap(headers: &hyper::header::HeaderMap) -> HashMap<String, Vec<String>> {
         let mut ret = std::collections::HashMap::new();
@@ -96,29 +94,43 @@ async fn handler(
 
     let mut p = tokio::process::Command::new(command)
         .args(args)
-        .env("HTTP_SH_URI", parts.uri.to_string())
+        .env("HTTP_SH_URI", req.uri().to_string())
         .env(
             "HTTP_SH_HEADERS",
-            serde_json::to_string(&headers_to_hashmap(&parts.headers)).unwrap(),
+            serde_json::to_string(&headers_to_hashmap(&req.headers())).unwrap(),
         )
         .stdin(std::process::Stdio::piped())
         .stdout(std::process::Stdio::piped())
         .spawn()
         .expect("failed to spawn");
 
-    let body = body.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e));
-    let mut body = tokio_util::io::StreamReader::new(body);
-    let mut stdin = p.stdin.take().expect("failed to open stdin");
+    let req_body = req
+        .into_body()
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e));
+    let mut req_body = tokio_util::io::StreamReader::new(req_body);
+    let mut stdin = p.stdin.take().expect("failed to take stdin");
+    let write_stdin = async {
+        tokio::io::copy(&mut req_body, &mut stdin)
+            .await
+            .expect("streaming request body");
+    };
 
-    let res_body = Body::wrap_stream(tokio_util::io::ReaderStream::new(p.stdout.unwrap()));
-    let resp = Response::builder()
-        .header("Content-Type", "text/plain")
-        .body(res_body);
+    let mut stdout = p.stdout.take().expect("failed to take stdout");
+    let res_body = Body::wrap_stream(tokio_util::io::ReaderStream::new(stdout));
+    let read_stdout = async {
+        Response::builder()
+            .header("Content-Type", "text/plain")
+            .body(res_body)
+            .expect("streaming response body")
+    };
 
-    tokio::io::copy(&mut body, &mut stdin)
-        .await
-        .expect("pipe failed");
-    return resp;
+    let (_, response) = tokio::join!(write_stdin, read_stdout);
+
+    tokio::spawn(async move {
+        p.wait().await.expect("process exited with an error");
+    });
+
+    response
 }
 
 async fn shutdown_signal() {
@@ -136,9 +148,7 @@ async fn test_handler() {
         .header("Last-Event-ID", 5)
         .body("zebody".into())
         .unwrap();
-    let resp = handler(req, &"bash".into(), &vec!["-c".into(), "cat".into()])
-        .await
-        .unwrap();
+    let resp = handler(req, &"bash".into(), &vec!["-c".into(), "cat".into()]).await;
     assert_eq!(resp.status(), StatusCode::OK);
     assert_eq!(resp.headers().get("content-type").unwrap(), "text/plain");
     let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
