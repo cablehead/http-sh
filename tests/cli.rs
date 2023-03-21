16633032cf8e1104b30e08ccc3772e6baf4a3a4c
Andy Gayton <andy@thecablelounge.com>
2023-03-21 07:47:55 -0700

    feat: add a normalized authority to the request meta-data (#32)

--- tests/cli.rs
+++ tests/cli.rs
@@ -25,6 +25,15 @@ fn cargo_bin(name: &str) -> std::path::PathBuf {
         .unwrap_or_else(|| target_dir().join(format!("{}{}", name, std::env::consts::EXE_SUFFIX)))
 }
 
+fn run_curl(args: Vec<&str>) -> std::process::Output {
+    let mut command = Command::new("curl");
+    command.arg("-s");
+    for arg in args {
+        command.arg(arg);
+    }
+    command.output().unwrap()
+}
+
 #[test]
 fn serve_unix() {
     let temp_dir = tempfile::tempdir().unwrap();
@@ -46,27 +55,48 @@ fn serve_unix() {
 
     // read startup log line to ensure serve is ready
     let stdout = serve.stdout.take().unwrap();
-    let mut stdout = std::io::BufReader::new(stdout);
-    let mut read = String::new();
-    stdout.read_line(&mut read).unwrap();
+    let stdout = std::io::BufReader::new(stdout);
+    let mut loglines = stdout.lines();
 
-    let got = Command::new("curl")
-        .arg("-s")
-        .arg("--unix-socket")
-        .arg(path)
-        .arg("http://a-host")
-        .output()
-        .unwrap();
+    let _logline = loglines.next().unwrap().unwrap();
+    // todo: assert start logline looks OK
+
+    let got = run_curl(vec![
+        "--http1.1",
+        "--unix-socket",
+        path,
+        "http://localhost:5555/",
+    ]);
     assert_eq!(want.as_bytes(), got.stdout);
+
+    // next , parse got.stdout to a Response and assert HOST header
+    let logline = loglines.next().unwrap().unwrap();
+    let log: http_sh::Request = serde_json::from_str(&logline).unwrap();
+    assert_eq!(log.proto, "HTTP/1.1");
+    assert_eq!(log.authority, Some("localhost:5555".to_string()));
+
+    let got = run_curl(vec![
+        "--http2-prior-knowledge",
+        "--unix-socket",
+        path,
+        "http://localhost:5555/",
+    ]);
+    assert_eq!(want.as_bytes(), got.stdout);
+    let logline = loglines.next().unwrap().unwrap();
+    assert_eq!(log.authority, Some("localhost:5555".to_string()));
 
     serve.kill().unwrap();
     // todo: graceful shutdown
     let _exit_status = serve.wait().unwrap();
     // println!("exit_status: {:?}", exit_status);
 
-    // read remaining logs
-    stdout.read_to_string(&mut read).unwrap();
-    // println!("remaining logs: {}", read);
+    let log: http_sh::Request = serde_json::from_str(&logline).unwrap();
+    assert_eq!(log.proto, "HTTP/2.0");
+
+    println!("remaining logs");
+    for line in loglines {
+        println!("{}", line.unwrap());
+    }
 
     let mut stderr = String::new();
     let n = serve
